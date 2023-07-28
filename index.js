207b687962e8036016315bcf6abcd0d0ca05d7e3
Jon A <jon.jumangi@gmail.com>
2023-07-28 10:26:48 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,79 @@
+//packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const path = require('path');
+
+//  an array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Whats the title for your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Whats a good description for your project?',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: "What is your GitHub username?",
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "Whatis your email address?",
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'What kind of license do you want? ',
+    choices: ["MIT", "Apache 2.0", "GNU v3", "BSD 2-Clause"]
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What command should install the dependencies?',
+    default: 'npm i',
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'What command should run tests?',
+    default: 'npm test',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What should the users know about using the repo?',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'What should the users know about contributing to the repo?',
+  },
+  {
+    type: 'input',
+    name: 'thanks',
+    message: 'Who would you like to give thanks to for this project?',
+  },
+]
+  
+
+// function to write README file
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+ }
+
+// TODO: Create a function to initialize app
+function init() { 
+inquirer.prompt(questions).then((inquirerResponses) =>{
+  console.log("Your README is being generated!");
+  writeToFile('README.md', generateMarkdown({...inquirerResponses}));
+});
+}
+
+// Function call to initialize app
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,54 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+## Description of Project
+
+ ${data.description}
+
+## Owner Credentials
+
+* ${data.github}
+* ${data.email}
+
+## License
+
+${data.license}
+
+## Installation
+
+The command "${data.installation}" is used to install the nessary dependencies.
+
+## Testing
+
+To run tests, use the command "${data.test}".
+
+## Usage
+
+${data.usage}
+
+## Contributing to the Repo
+
+${data.contributing}
+
+## Thanks
+
+${data.thanks}
+* Thank you for viewing this repo, please reach out if you have any other questions
+
+`;
+}
+
+module.exports = generateMarkdown;
